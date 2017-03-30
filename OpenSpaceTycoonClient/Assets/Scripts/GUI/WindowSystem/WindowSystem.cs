d951e336dc537787e46d43e810700a6d7e237564
McMuzzle <vezuvan@hotmail.com>
2017-03-30 18:10:24 +0200

    Ajout du systeme de Window

---
+++ OpenSpaceTycoonClient/Assets/Scripts/GUI/WindowSystem/WindowSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WindowSystem : MonoBehaviour {
+
+    [SerializeField] Window windowPrefab = null;
+    
+    public Window NewWindow(string name, GameObject data) {
+        Window newWindow = Instantiate<Window>(windowPrefab);
+        newWindow.transform.SetParent(transform);
+        newWindow.SetPosition(Vector2.zero);
+        newWindow.windowSystem = this;
+        data.transform.SetParent(newWindow.content);
+        RectTransform r = data.GetComponent<RectTransform>();
+        r.anchorMin = Vector2.zero;
+        r.anchorMax = Vector2.one;
+        r.offsetMin = Vector2.zero;
+        r.offsetMax = Vector2.zero;
+        return newWindow;
+    }
+
+    public void BringToFront(Window window){
+		window.transform.SetAsLastSibling ();        
+	}
+}
