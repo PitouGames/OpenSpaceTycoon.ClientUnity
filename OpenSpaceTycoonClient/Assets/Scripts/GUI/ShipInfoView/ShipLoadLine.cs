a5bfd13ec9f001da856ff5ab64bc87f1d069aec5
mcmuzzle <vezuvan@hotmail.com>
2017-06-15 15:11:05 +0200

    Ajout des chargements dans les infos du vaisseau

---
+++ OpenSpaceTycoonClient/Assets/Scripts/GUI/ShipInfoView/ShipLoadLine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShipLoadLine : MonoBehaviour {
+    private OSTData.ShipDestination.LoadData _data;
+
+    [SerializeField]
+    private TMPro.TextMeshProUGUI typeName = null;
+
+    [SerializeField]
+    private TMPro.TextMeshProUGUI qte = null;
+
+    public void Setdata(OSTData.ShipDestination.LoadData data) {
+        _data = data;
+        typeName.text = _data.type.ToString();
+        qte.text = _data.qte.ToString();
+    }
+}
